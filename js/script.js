62f9328caced72a5c9fe27118dfedd5e771577dd
Sumaiya Aktar <sumaiyaaktar3029@gmail.com>
2023-07-06 22:43:25 +0600

    add

--- js/script.js
+++ js/script.js
@@ -4,14 +4,25 @@ $(".slider-for").slick({
   arrows: false,
   fade: true,
   asNavFor: ".slider-nav",
+  autoplay: true,
 });
 $(".slider-nav").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   asNavFor: ".slider-for",
   dots: true,
-  centerMode: true,
+  centerMode: false,
   focusOnSelect: true,
   slidesToShow: 1,
   adaptiveHeight: true,
+  arrows: false,
+  autoplay: true,
+});
+
+$(".multiple-items").slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  arrows: false,
+  autoplay: true,
 });
