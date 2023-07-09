cdcfbb074c30898a35d9be73b1b4a6b7806c1a9f
Sumaiya Aktar <sumaiyaaktar3029@gmail.com>
2023-07-09 21:56:05 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
-$(".slider-for").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".slider-nav",
-  autoplay: true,
-});
-$(".slider-nav").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: ".slider-for",
-  dots: true,
-  centerMode: false,
-  focusOnSelect: true,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  arrows: false,
-  autoplay: true,
-});
+$(document).ready(function () {
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: ".slider-nav",
+    // autoplay: true,
+    nextArrow: '<button class="fa-solid fa-angle-right"></button>',
+    prevArrow: '<button class="fa-solid fa-angle-left"></button>',
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: true,
+    centerMode: false,
+    focusOnSelect: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    // autoplay: true,
+  });
 
-$(".multiple-items").slick({
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 3,
-  arrows: false,
-  autoplay: true,
+  $("#owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 5,
+    autoplay: true,
+    nav: false,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        dots:false,
+    
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
 });
